16352055744de60ffd524aec4677efbfa9a32987
chenps3 <chenps3@qq.com>
2021-02-08 16:49:25 +0800

    init

--- scg-demo/src/main/java/com/chenps3/openapipoc/scgdemo/RouteServerApplication.java
+++ scg-demo/src/main/java/com/chenps3/openapipoc/scgdemo/RouteServerApplication.java
@@ -1,2 +1,14 @@
-package com.chenps3.openapipoc.scgdemo;public class RouteServerApplication {
+package com.chenps3.openapipoc.scgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteServerApplication.class, args);
+    }
 }
+
+
